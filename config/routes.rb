58a9a3f65f5239fa505fc1b36c52c53c7125dda8
Bert Pareyn <bp323@caret.cam.ac.uk>
2011-08-05 17:44:30 +0100

    SAKIII-3759 Set up widget library page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SakaiWidgetlibrary::Application.routes.draw do
 
   match 'library' => 'library#index'
+  match 'library/:id' => 'library#index'
   root :to => "home#index"
 
   # The priority is based upon order of creation:
