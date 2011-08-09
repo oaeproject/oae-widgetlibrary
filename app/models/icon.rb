2fddc71a430000ffe131806fab16f5e77cc081a2
Chris Roby <christopher.roby@gmail.com>
2011-08-08 18:09:59 -0700

    SAKIII-3741 Changing sizes for the Icon attachment model, after reviewing the designs

--- app/models/icon.rb
+++ app/models/icon.rb
@@ -1,3 +1,3 @@
 class Icon < ActiveRecord::Base
-  has_attached_file :image, :styles => { :thumb => ["100x80!", :png], :large => ["800x800", :png] }
+  has_attached_file :image, :styles => { :thumb => ["50x50!", :png], :medium => ["100x100!", :png], :large => ["800x800", :png] }
 end
