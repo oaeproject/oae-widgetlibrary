f4373f701cf8d1f62ddcb4f26c902aed7c9c2da4
Chris Roby <christopher.roby@gmail.com>
2011-08-08 17:24:48 -0700

    SAKIII-3741 Create Rating model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110808232317) do
+ActiveRecord::Schema.define(:version => 20110809002224) do
+
+  create_table "ratings", :force => true do |t|
+    t.text     "review"
+    t.integer  "stars"
+    t.integer  "widget_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "widgets", :force => true do |t|
     t.string   "title"
