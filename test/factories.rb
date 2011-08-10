1f919368c02e725bd9a7d3fea30adcd980edf3a2
D. Stuart Freeman <stuart.freeman@et.gatech.edu>
2011-08-10 09:42:01 -0400

    SAKIII-3746 replace fixture stubs with factory stubs

---
+++ test/factories.rb
@@ -0,0 +1,20 @@
+Factory.define :category do |f|
+end
+
+Factory.define :icon do |f|
+end
+
+Factory.define :language do |f|
+end
+
+Factory.define :rating do |f|
+end
+
+Factory.define :screenshot do |f|
+end
+
+Factory.define :state do |f|
+end
+
+Factory.define :widget do |f|
+end
