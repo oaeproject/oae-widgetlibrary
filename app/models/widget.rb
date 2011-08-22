62c6b528f1b3f03a1b4b15da2fe9c92d68a8ac7b
Chris Roby <christopher.roby@gmail.com>
2011-08-22 12:35:16 -0700

    SAKIII-3827 Integrate devise for user authentication * Hooked up the registration page * Hooked up the sign up dialog * Modified some scss files to handle the structure of the useractions * Tabs -> Spaces on the scss files (sorry for the big diff there) * Moved out the Icon model as it was just causing troubles and providing no real value * I won't do big commits like this again if I can help it - this just turned out to be a lot of things that were co-dependent

--- app/models/widget.rb
+++ app/models/widget.rb
@@ -1,8 +1,16 @@
 class Widget < ActiveRecord::Base
-  has_one :icon
+  has_attached_file :icon, :styles => { :thumb => ["50x50!", :png], :medium => ["100x100!", :png], :large => ["800x800", :png] }
+  has_attached_file :code
+
   belongs_to :state
   has_many :ratings
   has_many :screenshots
   has_and_belongs_to_many :categories
   has_and_belongs_to_many :languages
+
+  # Validations
+  validates_attachment_presence :icon
+  validates_attachment_presence :code
+  validates_attachment_content_type :code, :content_type => [ 'application/zip', 'application/x-zip', 'application/x-zip-compressed', 'application/octet-stream', 'application/x-compress', 'application/x-compressed', 'multipart/x-zip' ],
+                                    :message => 'file must be a .zip file'
 end
