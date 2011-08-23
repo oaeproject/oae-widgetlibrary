e79487a84690c2e64732bfa85ff5109c99f65667
Bert Pareyn <bp323@caret.cam.ac.uk>
2011-08-23 13:32:57 +0100

    SAKIII-3918 Use jQuery Slides for the carousel

--- public/javascripts/homecarousel.js
+++ public/javascripts/homecarousel.js
@@ -1,50 +1,30 @@
 var homeCarouselContainer = "#widgetlibrary_carousel";
-var homeCarousel = "ul#widgetlibrary_homecarousel";
-var homeCarouselNavItem = ".widgetlibrary_homecarousel_navitem";
-var stopped = false;
+var homeCarousel = "#widgetlibrary_homecarousel";
+var carouselChoice = ".carousel_choice";
+var carouselChoiceArrow = ".carousel_choice_arrow";
 
-var showSelectedArrow = function(index){
-    $("#carousel_choice_arrow_" + (index.last)).animate({"opacity":"show"}, 500);
+var showSelectedArrow = function(){
+    $(carouselChoice + ".current").find(carouselChoiceArrow).animate({"opacity":"show"}, 500);
 };
 
-var handleArrows = function(index){
-    $(".carousel_choice_arrow:visible").animate({"opacity":"hide"}, 500, showSelectedArrow(index));
-};
-
-var addNavigationBinding = function(carousel){
-
-    $(homeCarouselContainer).css("visibility", "visible");
-
-    $(homeCarouselNavItem).click(function(){
-        var index = $(this).data("carousel-index");
-        carousel.scroll(index);
-        carousel.stopAuto();
-        stopped = true;
-    });
-
-    carousel.clip.hover(function() {
-        carousel.stopAuto();
-    }, function() {
-        if(!stopped){
-            carousel.startAuto();
-        }
-    });
-
-    carousel.clip.click(function() {
-        stopped = true;
-        carousel.stopAuto();
-    });
+var hidePrevArrow = function(){
+    $(carouselChoiceArrow + ":visible").animate({"opacity":"hide"}, 500);
 };
 
 var addBinding = function(){
-    $(homeCarousel).jcarousel({
-        auto: 10,
-        animation: "slow",
-        scroll: 1,
-        wrap: 'last',
-        initCallback: addNavigationBinding,
-        itemVisibleInCallback: handleArrows 
+    $(homeCarousel).slides({
+        preload: true,
+        generateNextPrev: false,
+        generatePagination: false,
+        slideSpeed: 800,
+        hoverPause: true,
+        pause: 2500,
+        play: 10000,
+        animationStart: hidePrevArrow,
+        animationComplete: showSelectedArrow
     });
+    showSelectedArrow();
+    $(homeCarousel).css("visibility", "visible");
 };
 
 $(document).ready(addBinding);
