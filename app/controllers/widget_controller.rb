bc138858ed725adcb2b80e79dc38dcac503b2d01
Chris Roby <christopher.roby@gmail.com>
2012-01-02 10:27:23 -0800

    NOJIRA fixing widget detail regression

--- app/controllers/widget_controller.rb
+++ app/controllers/widget_controller.rb
@@ -2,7 +2,7 @@ class WidgetController < ApplicationController
   def show
     widget_title = params[:widget_title]
     @widget = Widget.first( :conditions => { :url_title => widget_title } )
-    if (!@widget.state.title.eql? "accepted" && @widget.user != current_user) && !can_view_admin_area?
+    if !(@widget.state_id.eql?(State.accepted) && @widget.user != current_user) || (!@widget.state_id.eql?(State.accepted) && can_view_admin_area?)
       redirect_to :root
     end
     @related = Widget.order("random()").limit(5)
