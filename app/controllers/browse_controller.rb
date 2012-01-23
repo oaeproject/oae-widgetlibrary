3878fd05370eeea927bd4cf1fab51bf4a724c3c0
Chris Roby <christopher.roby@gmail.com>
2012-01-23 09:27:56 -0800

    OAEWDGT-77 Changing the featured behavior

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -12,10 +12,10 @@ class BrowseController < ApplicationController
       @widgets = Widget.includes(:version => :categories).where("widgets.active = ? AND versions.title LIKE '%#{params[:q]}%'", true).where("categories_versions.category_id = ?", @cat.id).order(order).page(params[:page])
     else
       @widgets = Widget.includes(:versions).where("widgets.active = ? AND versions.title LIKE '%#{params[:q]}%'", true).order(order).page(params[:page])
+      @featured = Widget.includes(:versions).where(args).order(order).limit(3)
     end
 
     @categories = Category.includes(:versions => :widget).where("widgets.active = ?", true)
-    @featured = Widget.includes(:versions).where(args).order(order).limit(3)
     if request.xhr?
       render :partial => "browse/livesearch"
     else
