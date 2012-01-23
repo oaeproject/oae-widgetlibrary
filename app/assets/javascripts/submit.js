35b1f6100f31b05ac8473173664e541cdcb741d6
Christian Vuerings <vueringschristian@gmail.com>
2012-01-23 11:57:18 -0800

    OAEWDGT-96 - Add loading to submit & registrations Jira: https://jira.sakaiproject.org/browse/OAEWDGT-96

--- app/assets/javascripts/submit.js
+++ app/assets/javascripts/submit.js
@@ -1,6 +1,26 @@
 //= require submit/newversion
 
 $(function() {
-    // Reset all the errors on a new submit of the form
-    $("#new_version").on("submit", WL.reset_errors);
+
+    var $submit_loading = $("#submit_loading");
+
+    $submit_loading.jqm({
+        modal: true,
+        overlay: 20,
+        toTop: true
+    });
+
+    var showLoading = function(){
+        $submit_loading.css({
+            "top": $("html").scrollTop() + 150
+        });
+
+        $submit_loading.jqmShow();
+    };
+
+    $("#new_version").on("submit", function(){
+        // Reset all the errors on a new submit of the form
+        WL.reset_errors();
+        showLoading();
+    });
 });
