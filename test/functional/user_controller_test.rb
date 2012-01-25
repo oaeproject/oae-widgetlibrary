54a2fd8c0cdfe49e383cbe0bce035ca39c8110be
Chris Roby <christopher.roby@gmail.com>
2012-01-25 14:49:54 -0800

    NOJIRA Add some user controller tests

--- test/functional/user_controller_test.rb
+++ test/functional/user_controller_test.rb
@@ -1,4 +1,20 @@
 require 'test_helper'
 
 class UserControllerTest < ActionController::TestCase
+  test "should find user" do
+    user = FactoryGirl.create(:user)
+    get :index, {:id => user.id, :url_title => user.url_title}
+    assert_equal user, assigns(:user), "@user was populated correctly"
+  end
+
+  test "should find widgets for a user" do
+    user = FactoryGirl.create(:user)
+    widget = FactoryGirl.create(:widget, :user_id => user.id)
+    version = FactoryGirl.create(:version, :widget_id => widget.id)
+    widget.version_id = version.id
+    widget.save
+
+    get :index, {:id => user.id, :url_title => user.url_title}
+    assert_equal 1, assigns(:widgets).size, "User's widgets were assigned"
+  end
 end
