4514bc127e854c7f2db9b3e441b2d973189ed357
Chris Roby <christopher.roby@gmail.com>
2012-01-25 14:47:57 -0800

    NOJIRA remove the video until we have one

--- app/assets/javascripts/sdk.js
+++ app/assets/javascripts/sdk.js
@@ -2,23 +2,24 @@
 
 $(function() {
 
-    var showVideoOverlay = function(){
-        $("#videocontent_overlay_container").css("margin-left", $(window).width() / 2 - 325);
-        $("#videocontent_overlay_container").css("margin-top", $(window).height() / 2 - 187);
-        $("#videocontent_overlay").animate({opacity:'toggle'}, 500, function(){
-            $("#videocontent_overlay_container").animate({opacity:'toggle'}, 500);
-        });
-    };
-
-    var hideVideoOverlay = function(){
-        $("#videocontent_overlay_container").animate({opacity:'toggle'}, 300, function(){
-            $("#videocontent_overlay").animate({opacity:'toggle'}, 300);
-        });
-    };
+    // Disabling these for now
+    // var showVideoOverlay = function(){
+    //     $("#videocontent_overlay_container").css("margin-left", $(window).width() / 2 - 325);
+    //     $("#videocontent_overlay_container").css("margin-top", $(window).height() / 2 - 187);
+    //     $("#videocontent_overlay").animate({opacity:'toggle'}, 500, function(){
+    //         $("#videocontent_overlay_container").animate({opacity:'toggle'}, 500);
+    //     });
+    // };
+    //
+    // var hideVideoOverlay = function(){
+    //     $("#videocontent_overlay_container").animate({opacity:'toggle'}, 300, function(){
+    //         $("#videocontent_overlay").animate({opacity:'toggle'}, 300);
+    //     });
+    // };
 
     var addBinding = function(){
-        $("#developer_videocontent_container").live("click", showVideoOverlay);
-        $("#videocontent_overlay").live("click", hideVideoOverlay);
+        // $("#developer_videocontent_container").live("click", showVideoOverlay);
+        // $("#videocontent_overlay").live("click", hideVideoOverlay);
         $(".expand_all_link").live("click", function(){
             var expanded = $(".expand_all_link").attr("data-expanded");
             if(expanded === "true"){
