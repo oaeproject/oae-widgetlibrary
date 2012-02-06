1737332016fedf29cbc4e4618fe07499f9d31f5f
Chris Roby <christopher.roby@gmail.com>
2012-02-06 14:12:56 -0800

    OAEWDGT-145 Calculate the thumbnail style so they don't have their aspect ratios screwed up

--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -3,7 +3,7 @@ class Screenshot < ActiveRecord::Base
 
   belongs_to :version
   has_attached_file :image, :styles => {
-    :thumb => ["100x57>", :png],
+    :thumb => [Proc.new { |instance| instance.thumb_style }, :png],
     :medium => ["540x310>", :png],
     :large => ["800x800>", :png]
   }
@@ -13,4 +13,17 @@ class Screenshot < ActiveRecord::Base
                             :less_than=>5.megabytes,
                             :message => 'file must be less than 5 megabytes',
                             :if => lambda { image.dirty? }
+
+  def thumb_style
+    geo = Paperclip::Geometry.from_file(image.to_file(:original))
+    ratio = 100.0/60.0
+    # if the ratio is higher than the container, that means the image is
+    # wider than the container, so we prioritize the height and let the width
+    # overflow as it would
+    if geo.width.to_f/geo.height.to_f > ratio
+      "x60"
+    else
+      "100x"
+    end
+  end
 end
