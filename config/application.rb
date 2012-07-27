4fb9b19646d135ff4d2d8b5849c343530b8dc7af
Christian Vuerings <vueringschristian@gmail.com>
2012-07-27 09:37:13 -0700

    OAEWDGT-181 - fix devise gem - try 1

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 require 'net/http'
+require 'devise'
 
 if (defined?(Bundler))
   Bundler.require *Rails.groups(:assets => %w(development test))
