9f399e0df28e8c3cd10acb5a3eba194357c6b15b
Mathieu Decoene <hello@mathieudecoene.be>
2013-04-25 17:07:20 +0100

    Added frontend docs + changed endpoints to newer ones

--- app/controllers/sdk_controller.rb
+++ app/controllers/sdk_controller.rb
@@ -7,8 +7,7 @@ class SdkController < ApplicationController
     # Properties
 
     @@url_readme_file = 'https://raw.github.com/sakaiproject/Hilary/master/README.md'
-    @@url_rest_module = 'http://oae.oaeproject.org/api/doc/module/'
-    @@url_rest_modules = 'http://oae.oaeproject.org/api/doc/modules/'
+    @@url_rest_api_doc = 'http://tenant1.oae.com/api/doc/'
 
     # Show index page of SDK
 
@@ -21,23 +20,41 @@ class SdkController < ApplicationController
 
     def sdk_section
         
+        # Properties
+        @modules = nil
+        
         # The requested template
         template = params[:section]   
-                      
+                              
         # If section is 'api'
         if template == 'api'
-            # Load the module names
-            @modules = get_doc_module_names
-            # Show the first module by default
-            if params[:subsection] == nil
-                @module_title = @modules.first
-                @content = get_doc_details(@modules.first)
-            # If a module is selected, load the module details
-            else                
-                @module_title = params[:subsection]
-                @content = get_doc_details(@module_title)
+                           
+            # Show front or back end    
+            if !params[:subsection].blank? 
+
+                # Load the module names                
+                @modules = get_doc_module_names(@@url_rest_api_doc + params[:subsection])     
+                
+                # Set module title
+                @subsection = params[:subsection]
+                                                
+                # Set the url for the HTTPRequest
+                if !params[:module].blank? 
+                    url = params[:module]
+                    @module_title = params[:module]
+                else 
+                    url = @modules.first
+                    @module_title = @modules.first 
+                end
+                
+                # Get the module details                                       
+                @content = get_doc_details(@@url_rest_api_doc + params[:subsection]  + '/' + url)                    
+
+                # Set the variables for rendering the page                
+                locals = { :modules => @modules, :subsection => @subsection, :content => @content, :title => @module_title }  
+                                                            
             end
-            locals = { :modules => @modules, :content => @content, :title => @module_title }
+                                                                            
         # If section is 'development-environment-setup', load the parse the markdown file
         elsif template == 'development-environment-setup'
             @content = parse_markdown
@@ -66,10 +83,11 @@ class SdkController < ApplicationController
 
     # Get all the modules from the REST API
     #
-    # @return   {Array}     arrData       The array containing all the module names
+    # @params   {String}    url             The endpoint of the documentation api
+    # @return   {Array}     arrData         The array containing all the module names
 
-    def get_doc_module_names
-        resp = Net::HTTP.get_response(URI.parse(@@url_rest_modules))
+    def get_doc_module_names(url)
+        resp = Net::HTTP.get_response(URI.parse(url))
         data = resp.body        
         if !resp.body.is_a?(Net::HTTPSuccess)
             data = ActiveSupport::JSON.decode(resp.body).sort
@@ -80,11 +98,11 @@ class SdkController < ApplicationController
 
     # Get the module details from the REST API
     #
-    # @param    {String}    m              The requested module
+    # @param    {String}    url            The url for the HTTPRequest
     # @return   {Hash}      *              The hash containing the module details
 
-    def get_doc_details(m)
-        resp = Net::HTTP.get_response(URI.parse(@@url_rest_module + m))                     
+    def get_doc_details(url)   
+        resp = Net::HTTP.get_response(URI.parse(url))                             
         return Yajl::Parser.parse(resp.body)
     end
 end
